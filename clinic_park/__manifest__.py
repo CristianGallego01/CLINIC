1e36091211a4c53e950978d9ce5fabf78c6c687a
zowwii <cristiancmgallego@gmail.com>
2025-04-10 17:52:52 -0500

    6

--- clinic_park/__manifest__.py
+++ clinic_park/__manifest__.py
@@ -3,10 +3,11 @@
     'version': '1.0.1',
     'depends': ['base', 'mail'],
     'data': [
-        'security/ir.model.access.csv',  # Carga primero los permisos de acceso
+        'security/ir.model.access.csv', 
+        'views/clinic_park_menus.xml',
         'views/clinic_park_patient_view.xml',
         'views/clinic_park_triage_view.xml',
-        'views/clinic_park_menus.xml',
+
 
     ],
     'application': True,
