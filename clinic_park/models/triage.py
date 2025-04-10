3b8a79ae2499197023396374085b6aec983f164e
zowwii <cristiancmgallego@gmail.com>
2025-04-10 17:24:32 -0500

    vistas2

--- clinic_park/models/triage.py
+++ clinic_park/models/triage.py
@@ -8,9 +8,9 @@ class ClinicParkTriage(models.Model):
     patient_id = fields.Many2one('clinic.park.patient', string='Paciente', required=True, ondelete='cascade')
     current_date= fields.Date(string='Fecha', default=fields.Date.context_today, required=True)
     reason = fields.Text(string='Motivo de Consulta', required=True)
-    name = fields.Char(string='Nombre', related='paciente_id.name', store=True)
-    age = fields.Integer(string='Edad', related='paciente_id.age', store=True)
-    dni = fields.Char(string='Cedula', related='paciente_id.dni', store=True)
+    name = fields.Char(string='Nombre', related='patient_id.name', store=True)
+    age = fields.Integer(string='Edad', related='patient_id.age', store=True)
+    dni = fields.Char(string='Cedula', related='patient_id.dni', store=True)
     # signos vitales
     temperatura = fields.Float(string='Temperatura', required=True)
     pulso = fields.Integer(string='Pulso', required=True)
