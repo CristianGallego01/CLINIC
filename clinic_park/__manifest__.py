373847ef4fa152710ece7cb164c0057d5d0eaa5c
zowwii <cristiancmgallego@gmail.com>
2025-04-10 17:29:13 -0500

    views sin s

--- clinic_park/__manifest__.py
+++ clinic_park/__manifest__.py
@@ -4,8 +4,8 @@
     'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',  # Carga primero los permisos de acceso
-        'views/clinic_park_patient_views.xml',
-        'views/clinic_park_triage_views.xml',
+        'views/clinic_park_patient_view.xml',
+        'views/clinic_park_triage_view.xml',
         'views/clinic_park_menus.xml',
 
     ],
