266ace12bfcbe71405b6633c53034d5353c2283e
zowwii <cristiancmgallego@gmail.com>
2025-04-10 13:24:19 -0500

    inicio

---
+++ clinic_park/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "clinic_park",
+    'version': '1.0.1',
+    "depends": ["base","account"
+                
+    ],
+    "data": [
+
+    ],
+    "application": True,
+    "installable": True,
+}
