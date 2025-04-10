088e8ef96c0618ac60ee5810c7babd9e552e1960
zowwii <cristiancmgallego@gmail.com>
2025-04-10 14:44:18 -0500

    otra vez el csv

--- clinic_park/__manifest__.py
+++ clinic_park/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.0.1',
     'depends': ['base', 'account'],
     'data': [
+        'security/ir.model.access.csv',  # Carga primero los permisos de acceso
         'views/clinic_park_views.xml',       # Carga primero vistas/modelos
         'views/clinic_park_menus.xml',
 
