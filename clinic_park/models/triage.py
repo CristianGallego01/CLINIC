4d7761bf2ef39abe54012daed39cef1ff6dc3b6e
zowwii <cristiancmgallego@gmail.com>
2025-04-11 04:20:05 -0500

    envio?

--- clinic_park/models/triage.py
+++ clinic_park/models/triage.py
@@ -26,11 +26,48 @@ class ClinicParkTriage(models.Model):
 
     patient_id = fields.Many2one('clinic.park.patient', string='Paciente')
 
+    # @api.model
+    # def create(self, vals):
+    #     record = super().create(vals)
+    #     if vals.get('atencion') == 'consulta':
+    #         record._crear_consulta_si_aplica()
+    #     return record
+
+    # def write(self, vals):
+    #     anterior_clasificacion = self.mapped('atencion')
+    #     res = super().write(vals)
+
+    #     for record, anterior in zip(self, anterior_clasificacion):
+    #         nueva = vals.get('atencion', record.atencion)
+
+    #         # Si cambió de 'consulta' a otra → eliminar consulta
+    #         if anterior == 'consulta' and nueva != 'consulta':
+    #             consulta = self.env['clinic.park.consultations'].search([('triage_id', '=', record.id)])
+    #             consulta.unlink()
+
+    #         # Si cambió a 'consulta' → crear consulta
+    #         if anterior != 'consulta' and nueva == 'consulta':
+    #             record._crear_consulta_si_aplica()
+
+    #     return res
+
+    # def _crear_consulta_si_aplica(self):
+    #     self.ensure_one()
+    #     Consulta = self.env['clinic.park.consultations']
+    #     ya_existe = Consulta.search([('triage_id', '=', self.id)], limit=1)
+    #     if not ya_existe:
+    #         Consulta.create({
+    #             'triage_id': self.id,
+    #             'patient_id': self.patient_id.id,
+    #         })
+    
     @api.model
     def create(self, vals):
         record = super().create(vals)
         if vals.get('atencion') == 'consulta':
             record._crear_consulta_si_aplica()
+        elif vals.get('atencion') == 'preparacion':
+            record._crear_preparacion_si_aplica()
         return record
 
     def write(self, vals):
@@ -40,14 +77,23 @@ class ClinicParkTriage(models.Model):
         for record, anterior in zip(self, anterior_clasificacion):
             nueva = vals.get('atencion', record.atencion)
 
-            # Si cambió de 'consulta' a otra → eliminar consulta
+            # Si se cambia de consulta a otra
             if anterior == 'consulta' and nueva != 'consulta':
                 consulta = self.env['clinic.park.consultations'].search([('triage_id', '=', record.id)])
                 consulta.unlink()
 
-            # Si cambió a 'consulta' → crear consulta
+            # Si se cambia a consulta
             if anterior != 'consulta' and nueva == 'consulta':
                 record._crear_consulta_si_aplica()
+
+            # Si se cambia de preparación a otra
+            if anterior == 'preparacion' and nueva != 'preparacion':
+                preparacion = self.env['clinic.park.prepared'].search([('triage_id', '=', record.id)])
+                preparacion.unlink()
+
+            # Si se cambia a preparación
+            if anterior != 'preparacion' and nueva == 'preparacion':
+                record._crear_preparacion_si_aplica()
 
         return res
 
@@ -57,6 +103,16 @@ class ClinicParkTriage(models.Model):
         ya_existe = Consulta.search([('triage_id', '=', self.id)], limit=1)
         if not ya_existe:
             Consulta.create({
+                'triage_id': self.id,
+                'patient_id': self.patient_id.id,
+            })
+
+    def _crear_preparacion_si_aplica(self):
+        self.ensure_one()
+        Preparacion = self.env['clinic.park.prepared']
+        ya_existe = Preparacion.search([('triage_id', '=', self.id)], limit=1)
+        if not ya_existe:
+            Preparacion.create({
                 'triage_id': self.id,
                 'patient_id': self.patient_id.id,
             })
