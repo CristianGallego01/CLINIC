c42894be11a7660f8e6622c274977ddf258a53c0
zowwii <cristiancmgallego@gmail.com>
2025-04-10 23:50:51 -0500

    cheklist

--- clinic_park/models/preparation.py
+++ clinic_park/models/preparation.py
@@ -11,6 +11,22 @@ class ClinicParkPreparation(models.Model):
     preoperative_form = fields.Text(string='Formulario Preoperatorio', required=True)
     supplies = fields.Text(string='Insumos Utilizados', required=True)
     nurse_signature = fields.Char(string='Firma de Enfermería', required=True)
-    checklist_verified = fields.Boolean(string='Lista de Chequeo Verificada', default=False)
+    checklist_consent = fields.Boolean(string='Consentimiento informado firmado')
+    checklist_exams = fields.Boolean(string='Exámenes preoperatorios revisados')
+    checklist_equipment = fields.Boolean(string='Equipo quirúrgico preparado')
+    checklist_allergies = fields.Boolean(string='Alergias conocidas verificadas')
+    checklist_documents = fields.Boolean(string='Documentación completa')
 
     preparation_date = fields.Datetime(string='Fecha de Preparación', required=True)
+    patient_signature = fields.Binary(string='Firma del Paciente' required=True)
+
+    @fields.depends('checklist_consent', 'checklist_exams', 'checklist_equipment', 'checklist_allergies', 'checklist_documents')
+    def _compute_checklist_verified(self):
+        for rec in self:
+            rec.checklist_verified = all([
+                rec.checklist_consent,
+                rec.checklist_exams,
+                rec.checklist_equipment,
+                rec.checklist_allergies,
+                rec.checklist_documents
+            ])
