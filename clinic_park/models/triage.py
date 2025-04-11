cfeaf59e965bf1b74f64f02ceae52b57e6fccd9c
zowwii <cristiancmgallego@gmail.com>
2025-04-11 03:23:32 -0500

    correciones

--- clinic_park/models/triage.py
+++ clinic_park/models/triage.py
@@ -6,7 +6,7 @@ class ClinicParkTriage(models.Model):
 
     patient_id = fields.Many2one('clinic.park.patient', string='Paciente', required=True, ondelete='cascade')
     current_date= fields.Date(string='Fecha', default=fields.Date.context_today, required=True)
-    reason = fields.Text(string='Motivo de Consulta', required=True)
+    reason = fields.Text(string='Motivo de Consulta', related ="patient_id.reason",required=True)
     name = fields.Char(string='Nombre', related='patient_id.name', store=True)
     age = fields.Integer(string='Edad', related='patient_id.age', store=True)
     dni = fields.Char(string='Cedula', related='patient_id.dni', store=True)
@@ -16,12 +16,12 @@ class ClinicParkTriage(models.Model):
     presion = fields.Char(string='Presion', required=True)
     # clasificacion
     atencion = fields.Selection([ 
-        ('por clasificar', 'A.Por Clasificar'),
-        ('consulta', 'B.Consulta'),
-        ('preparacion', 'C.Preparación Quirúrgica'),
-        ('cirugia', 'D.Cirugía'),
-        ('recuperacion', 'E.Recuperación y Hospitalización'),
-        ('facturacion', 'F.Facturación'),
+        ('por clasificar', 'Por Clasificar'),
+        ('consulta', 'Consulta'),
+        ('preparacion', 'Preparación Quirúrgica'),
+        ('cirugia', 'Cirugía'),
+        ('recuperacion', 'Recuperación y Hospitalización'),
+        ('facturacion', 'Facturación'),
     ],default="por clasificar",string="clasificacion",copy=False,required=True)
 
     patient_id = fields.Many2one('clinic.park.patient', string='Paciente')
