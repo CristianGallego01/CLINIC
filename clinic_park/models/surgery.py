0277f499c9d827c5bd0dffd6444f19be0e2adcdc
zowwii <cristiancmgallego@gmail.com>
2025-04-11 00:41:34 -0500

    surgery

--- clinic_park/models/surgery.py
+++ clinic_park/models/surgery.py
@@ -8,5 +8,13 @@ class ClinicParkCirugia(models.Model):
     patient_id = fields.Many2one('clinic.park.patient', string='Paciente', required=True)
     name= fields.Char(string='Nombre', related='patient_id.name', store=True)
     dni = fields.Char(string='Cedula', related='patient_id.dni', store=True)
-    procedimiento = fields.Text(string='Procedimiento')
-    fecha_cirugia = fields.Datetime(string='Fecha de Cirugía')
+    
+    documents = fields.Binary(string='Documento Quirúrgico')
+    procedure_details = fields.Text(string='Detalles del Procedimiento')
+    surgery_date = fields.Datetime(string='Fecha de Cirugía')
+    documents = fields.Binary(string='Documento Quirúrgico')
+    vital_signs_notes = fields.Binary(string='Signos Vitales Intraoperatorios')
+    anesthesia_type = fields.select([
+        ('general', 'General'),
+        ('local', 'Local'),
+    ], string='Tipo de Anestesia', required=True)
