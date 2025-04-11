93c1e748e68f7d139c7ab86bc0cbdfa3eb3ab165
zowwii <cristiancmgallego@gmail.com>
2025-04-10 21:01:27 -0500

    relaciones2

--- clinic_park/models/surgery.py
+++ clinic_park/models/surgery.py
@@ -6,5 +6,7 @@ class ClinicParkCirugia(models.Model):
 
     triage_id = fields.Many2one('clinic.park.triage', string='Triage', required=True, ondelete='cascade')
     patient_id = fields.Many2one('clinic.park.patient', string='Paciente', required=True)
+    name= fields.Char(string='Nombre', related='patient_id.name', store=True)
+    dni = fields.Char(string='Cedula', related='patient_id.dni', store=True)
     procedimiento = fields.Text(string='Procedimiento')
     fecha_cirugia = fields.Datetime(string='Fecha de Cirugía')
